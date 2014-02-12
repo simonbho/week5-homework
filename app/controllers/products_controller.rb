8799bc67481056a46e466065fd2bc15b552e29d1
simonbho <sho2015@kellogg.northwestern.edu>
2014-02-12 08:27:03 -0600

    HW complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @product_list = []
+    @product_list = Product.all
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140212135302) do
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.string   "image_url"
+    t.integer  "price"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
